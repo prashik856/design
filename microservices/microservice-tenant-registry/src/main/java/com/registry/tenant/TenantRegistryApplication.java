57e9bf6071b404efe45436b35ad4a0fad53bb757
Prashik <prashik.raut@oracle.com>
2023-07-18 16:45:43 +0530

    Completed simple miroservice which uses database

--- microservices/microservice-tenant-registry/src/main/java/com/registry/tenant/TenantRegistryApplication.java
+++ microservices/microservice-tenant-registry/src/main/java/com/registry/tenant/TenantRegistryApplication.java
@@ -1,6 +1,8 @@
 package com.registry.tenant;
 
+import com.registry.tenant.dao.Persistence;
 import com.registry.tenant.models.Config;
+import com.registry.tenant.models.Tenant;
 import com.registry.tenant.utils.ConfigReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -29,6 +32,12 @@ class TenantRegistryApplication {
         logger.info("Creating application config bean");
         getConfig();
 
+        logger.info("Getting list of tenants to cache");
+        getExistingTenants();
+
+        logger.info("Creating hashmap of tenants");
+        cacheExistingTenants();
+
         SpringApplication.run(TenantRegistryApplication.class, args);
     }
 
@@ -36,5 +45,27 @@ class TenantRegistryApplication {
     public static Config getConfig() throws IOException {
         String configFilePath = "src/main/resources/application.yaml";
         return new ConfigReader(configFilePath).readConfig();
+    }
+
+    @Bean
+    public static List<Tenant> getExistingTenants()
+            throws IOException, SQLException, ClassNotFoundException {
+        String configFilePath = "src/main/resources/application.yaml";
+        Config config = new ConfigReader(configFilePath).readConfig();
+
+        return Persistence.getTenants(config);
+    }
+
+    @Bean
+    public static HashMap<String, Tenant> cacheExistingTenants()
+            throws IOException, SQLException, ClassNotFoundException {
+        String configFilePath = "src/main/resources/application.yaml";
+        Config config = new ConfigReader(configFilePath).readConfig();
+        List<Tenant> tenants = Persistence.getTenants(config);
+        HashMap<String, Tenant> cachedTenants = new HashMap<>();
+        for(Tenant tenant: tenants) {
+            cachedTenants.putIfAbsent(tenant.getNameSpace(), tenant);
+        }
+        return cachedTenants;
     }
 }
--- microservices/microservice-tenant-registry/src/main/java/com/registry/tenant/rest/TenantResource.java
+++ microservices/microservice-tenant-registry/src/main/java/com/registry/tenant/rest/TenantResource.java
@@ -3,16 +3,14 @@ package com.registry.tenant.rest;
 import com.registry.tenant.dao.Persistence;
 import com.registry.tenant.models.Config;
 import com.registry.tenant.models.Tenant;
-import com.registry.tenant.response.TenantResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -29,6 +27,18 @@ public class TenantResource {
     private Config config;
 
     /**
+     * Autowired list of all tenants.
+     * */
+    @Autowired
+    private List<Tenant> allTenants;
+
+    /**
+     * Autowired hash of all tenants. the cache.
+     * */
+    @Autowired
+    private HashMap<String, Tenant> cachedTenants;
+
+    /**
      * The logger.
      * */
     public static Logger logger = Logger.getLogger(TenantResource.class.getName());
@@ -40,56 +50,105 @@ public class TenantResource {
      * @return tenant
      * */
     @GetMapping("/api/v1/registry/tenants")
-    public TenantResponse getTenant(@RequestParam(value="namespace", defaultValue = "") String namespace) {
-        List<Tenant> tenants;
-        TenantResponse tenantResponse = new TenantResponse();
-        try {
-            tenants = Persistence.getTenants(config);
-            if (Objects.equals(namespace, "")) {
-                logger.info("Parameter namespace value is empty. ");
-            } else {
-                Tenant responseTenant = null;
-                logger.info("Namespace value is non empty.");
-                // namespace is unique
-                for (Tenant tenant : tenants) {
-                    if (Objects.equals(tenant.getNameSpace(), namespace)) {
-                        responseTenant = tenant;
-                        break;
-                    }
-                }
-                tenants.clear();
-                if(!Objects.equals(responseTenant, null)) {
-                    tenants.add(responseTenant);
+    public ResponseEntity getTenant(@RequestParam(value="namespace", defaultValue = "") String namespace) {
+        if (Objects.equals(namespace, "")) {
+            logger.info("Parameter namespace value is empty. ");
+        } else {
+            Tenant responseTenant = null;
+            logger.info("Namespace value is non empty.");
+            // namespace is unique
+            for (Tenant tenant : allTenants) {
+                if (Objects.equals(tenant.getNameSpace(), namespace)) {
+                    responseTenant = tenant;
+                    break;
                 }
             }
-
-            // Build Proper Response
-            createResponse(tenantResponse,
-                    200,
-                    "OK",
-                    tenants);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            createResponse(tenantResponse,
-                    500,
-                    "SQL Exception. Service not available.",
-                    new ArrayList<>());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            createResponse(tenantResponse,
-                    500,
-                    "ClassNotFoundException. Service not available.",
-                    new ArrayList<>());
+            if(!Objects.equals(responseTenant, null)) {
+                logger.info("Response non empty.");
+                return ResponseEntity.ok(responseTenant);
+            } else {
+                // Object is empty
+                return ResponseEntity.ok(new ArrayList<>());
+            }
         }
-        return tenantResponse;
+        // Build Proper Response
+        return ResponseEntity.ok(allTenants);
     }
 
-    private void createResponse(TenantResponse tenantResponse,
-                                int status,
-                                String reason,
-                                List<Tenant> data) {
-        tenantResponse.setStatus(status);
-        tenantResponse.setReason(reason);
-        tenantResponse.setData(data);
+    /**
+     * update tenant method. Used to create/update a tenant.
+     * If tenant is already present in cache, update the tenant else create a new one and update cache.
+     *
+     * @param tenant the incoming tenant
+     * @return ResponseEntity the http response entity
+     * */
+    @PutMapping("/api/v1/registry/tenants")
+    public ResponseEntity updateTenant(@Validated @RequestBody Tenant tenant) {
+        logger.info("PUT request to update/create tenant: " + tenant.toString());
+        // Check if tenant is already present in cache
+        if (cachedTenants.containsKey(tenant.getNameSpace())) {
+            logger.info("Tenant is already present. Updating tenant.");
+            tenant.setId(cachedTenants
+                    .get(tenant.getNameSpace()).getId());
+            try {
+                // update persistence.
+                Persistence.updateTenant(config, tenant);
+                // update cache.
+                cachedTenants.replace(tenant.getNameSpace(), tenant);
+                // update list of tenants
+                allTenants = cachedTenants.values().stream().toList();
+                // return ok
+                return ResponseEntity.ok(tenant);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+                logger.info("Not able to update tenant.");
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
+            logger.info("Creating new tenant entry");
+            UUID id = UUID.randomUUID();
+            tenant.setId(id.toString());
+            try {
+                // add new instance of tenant in persistence
+                Persistence.createTenant(config, tenant);
+                // update cache
+                cachedTenants.putIfAbsent(tenant.getNameSpace(), tenant);
+                // update list of tenants
+                allTenants = cachedTenants.values().stream().toList();
+                // return ok
+                return ResponseEntity.ok(tenant);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+                logger.info("Not able to create tenant.");
+                return ResponseEntity.internalServerError().build();
+            }
+        }
+    }
+
+    /**
+     *
+     * */
+    @DeleteMapping("/api/v1/registry/tenants")
+    public ResponseEntity deleteTenant(@NonNull @RequestParam(value = "namespace") String namespace) {
+        logger.info("Delete tenant with namespace: " + namespace);
+        if(cachedTenants.containsKey(namespace)) {
+            logger.info("Deleting tenant " + namespace);
+            try {
+                // update persistence
+                Persistence.deleteTenant(config, cachedTenants.get(namespace).getId());
+                // update cache
+                cachedTenants.remove(namespace);
+                // update list of tenants
+                allTenants = cachedTenants.values().stream().toList();
+                // return ok
+                return ResponseEntity.ok().build();
+            } catch (SQLException | ClassNotFoundException e) {
+                logger.info("Error deleting tenant");
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
+            logger.info("Tenant is not present is cache. It might not even be there.");
+            return ResponseEntity.notFound().build();
+        }
     }
 }
